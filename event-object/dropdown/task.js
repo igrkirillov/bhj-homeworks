bc27707dfa23a894b0ff358a6b8012dee6e46a39
Igor Kirillov <Igor.Kirillov@x5.ru>
2023-11-11 14:11:08 +0300

    dropdown доработка, чтобы страница не перегружалась

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,10 +1,19 @@
 const dropdowns = Array.from(document.querySelectorAll("div.dropdown"));
 for (const dropdown of dropdowns) {
     dropdown.addEventListener("click", () => onClickDropdown.call(dropdown));
+
     const dropdownItems = Array.from(document.querySelectorAll("li.dropdown__item"))
         .filter(el => el.closest("div.dropdown") === dropdown);
     for (const dropdownItem of Array.from(dropdownItems)) {
         dropdownItem.addEventListener("click", () => onClickDropdownItem.call(dropdownItem));
+    }
+
+    const dropdownLinks = Array.from(document.querySelectorAll("li.dropdown__item a.dropdown__link"))
+        .filter(el => el.closest("div.dropdown") === dropdown);
+    for (const dropdownLink of Array.from(dropdownLinks)) {
+        dropdownLink.onclick = event => {
+            event.preventDefault();
+        };
     }
 }
 
